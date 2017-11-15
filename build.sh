1fcdc94372dd175e654231c9c13484563436f79f
Raghu Udiyar <raghusiddarth@gmail.com>
2017-11-15 15:27:38 +0530

    HSFT: Revert "TOOLS-1820 Remove deprecated mongooplog tool"
    This reverts commit 75f2d994ce4bdcfca79d1b5e50c7d5125a6ab092.
    
    Mongooplog is useful for streaming oplog replication and keeping two
    mongodb nodes / clusters in sync; Useful for PoCs. Unfortunately this
    isn't available with mongobackup/restore, and doesn't seem to be planned
    in the future :
    https://groups.google.com/forum/#!searchin/mongodb-dev/mongooplog%7Csort:date/mongodb-dev/p1iqAth4CSg/mgejbD_BBgAJ

--- build.sh
+++ build.sh
@@ -22,6 +22,7 @@ mkdir -p bin
 
 ec=0
 for i in bsondump mongostat mongofiles mongoexport mongoimport mongorestore mongodump mongotop mongoreplay; do
+for i in bsondump mongostat mongofiles mongoexport mongoimport mongorestore mongodump mongotop mongooplog mongoreplay; do
         echo "Building ${i}..."
         go build -o "bin/$i" -tags "$tags" "$i/main/$i.go" || { echo "Error building $i"; ec=1; break; }
         ./bin/$i --version | head -1
